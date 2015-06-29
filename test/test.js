8bc2f072524ecde26480cd442cd04f0260d1381f
kengz <kengzwl@gmail.com>
2015-06-29 14:45:30 -0400

    added tests

---
+++ test/test.js
@@ -0,0 +1,15 @@
+// chai assertation library
+var chai = require('chai'),
+    should = chai.should()
+
+var bot = require(__dirname + '/../bot.js');
+
+//==============================================
+suite('Parameters setting', function() {
+    suite('token', function() {
+        var Alice = new bot(123456);
+        test('shall be in this.token', function() {
+            Alice.token.should.equal(123456);
+        })
+    })
+})
